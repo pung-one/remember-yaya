9067d071ef1cd9e000f3ac1f82878370968bacf0
pung-one <p-ungerer@gmx.net>
2023-04-20 18:23:03 +0200

    add blogpost component and add data-array with old posts and add images

--- components/BlogPost/index.js
+++ components/BlogPost/index.js
@@ -1,3 +1,21 @@
-export default function BlogPost() {
-  return <></>;
+import styled from "styled-components";
+import Image from "next/image";
+
+export default function BlogPost({ title, date, imagesrc, article }) {
+  return (
+    <>
+      <Title>{title}</Title>
+      <Date>{date}</Date>
+      <StyledImage src={imagesrc} alt={title} width={200} height={150} />
+      <Article>{article}</Article>
+    </>
+  );
 }
+
+const Title = styled.title``;
+
+const Date = styled.span``;
+
+const Article = styled.article``;
+
+const StyledImage = styled(Image)``;
