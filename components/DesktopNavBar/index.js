c906ee38618c62bd9f6f769a4983861442694817
pung-one <p-ungerer@gmx.net>
2023-04-26 16:10:54 +0200

    add about-page and navigation for desktop and mobile view

---
+++ components/DesktopNavBar/index.js
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+import Link from "next/link";
+
+export default function DesktopNavBar() {
+  return (
+    <NavContainer>
+      <List>
+        <ListElement>
+          <StyledLink href={"/"}>Home</StyledLink>
+        </ListElement>
+        <ListElement>
+          <StyledLink href={"/about"}>About</StyledLink>
+        </ListElement>
+      </List>
+    </NavContainer>
+  );
+}
+
+const NavContainer = styled.nav`
+  position: relative;
+  margin: 0 auto 0 auto;
+  background-color: black;
+`;
+
+const List = styled.ul`
+  position: relative;
+  display: flex;
+  align-items: center;
+  list-style: none;
+  gap: 3vw;
+  background-color: black;
+`;
+
+const ListElement = styled.li`
+  background-color: black;
+`;
+
+const StyledLink = styled(Link)`
+  background-color: black;
+  color: #00b49b;
+  &:hover {
+    color: white;
+  }
+  transition: all 0.2s;
+`;
--- components/Layout/index.js
+++ components/Layout/index.js
@@ -1,10 +1,55 @@
 import Header from "../Header";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import styled from "styled-components";
+import MobileNavBar from "../MobileNavBar";
 
 export default function Layout({ children, language, onToggleLanguage }) {
+  const [showNav, setShowNav] = useState(false);
+  const [viewportWidth, setViewportWidth] = useState("");
+  const [viewportHeight, setViewportHeight] = useState("");
+
+  const router = useRouter();
+  const route = router.route;
+
+  function handleToggleNav() {
+    setShowNav(!showNav);
+  }
+
+  function handleResize() {
+    setViewportWidth(window.innerWidth);
+    setViewportHeight(window.innerHeight);
+  }
+
+  useEffect(() => {
+    setShowNav(false);
+  }, [route]);
+
+  useEffect(() => {
+    if (typeof window !== undefined) {
+      setViewportWidth(window.innerWidth);
+      setViewportHeight(window.innerHeight);
+      window.addEventListener("resize", handleResize);
+      return () => window.removeEventListener("resize", handleResize);
+    }
+  }, []);
+
   return (
-    <>
-      <Header language={language} onToggleLanguage={onToggleLanguage} />
+    <LayoutContainer>
+      <Header
+        language={language}
+        onToggleLanguage={onToggleLanguage}
+        onToggleNav={handleToggleNav}
+        showNav={showNav}
+      />
+      {viewportHeight > viewportWidth && <MobileNavBar showNav={showNav} />}
       {children}
-    </>
+    </LayoutContainer>
   );
 }
+
+const LayoutContainer = styled.section`
+  position: relative;
+  height: 100%;
+  overflow-x: hidden;
+`;
